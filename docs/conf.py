5765fc482f5de0a562d4cf2a7bf40ddb63321471
Malcolm Nihlén <script_coded@hotmail.com>
2018-05-31 12:44:18 +0200

    Let's try again........

--- docs/conf.py
+++ docs/conf.py
@@ -20,8 +20,8 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Node Baseline'
-copyright = "2018, Malcolm Nihl\u00E9n"
-author = "Malcolm Nihl\u00E9n"
+copyright = "2018, Malcolm Nihlu'\u00E9'n"
+author = "Malcolm Nihlu'\u00E9'n"
 
 # The short X.Y version
 version = ''
@@ -129,7 +129,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'NodeBaseline.tex', 'Node Baseline Documentation',
-     "Malcolm Nihl\u00E9n", 'manual'),
+     "Malcolm Nihlu'\u00E9'n", 'manual'),
 ]
 
 
